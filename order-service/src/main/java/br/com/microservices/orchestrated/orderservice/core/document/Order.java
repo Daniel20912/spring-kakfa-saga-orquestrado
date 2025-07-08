f61bc32271b1a9b3186d1a2a37e3c804340b50ea
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-07-08 16:34:08 -0300

    Mapeamento dos DTOs, Document e Repositories do OrderService

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,8 +14,12 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collation = "order")
+// apenas Order e Event precisam estar mapeados como documentos, pois os outros objetos não terão uma collection só deles
+// eles apenas farão parte do documento
 public class Order {
 
+    @Id
     private String id;
     private List<OrderProducts> products;
     private LocalDateTime createdAt;
